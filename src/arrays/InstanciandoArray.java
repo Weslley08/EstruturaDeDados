aa0b67dfd07a67abe000a6c416b2ca6af3a76a0d
Weslley08 <weslleyjonathas88@gmail.com>
2021-05-20 17:03:43 -0300

    Adicionando o pacote 'arrays' e suas classes

---
+++ src/arrays/InstanciandoArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class InstanciandoArray {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		// Declarando array
+		int Array[]; 
+		
+		// Alocando memória para o array
+		int[] meuArray = new int[10]; 
+
+		// Combinando as duas instruções em uma
+		int[] intArray = new int[20];
+	}
+
+}
